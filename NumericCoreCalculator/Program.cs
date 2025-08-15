83ac168f4d5d44ba20976b688cd0d71957efc3b3
Jon Bradford <548416+jbradford@users.noreply.github.com>
2025-08-15 18:57:00 +0100

    Setup codestyles (#3)
    This PR moves to SDK 9, which has better codestyle support.
    Added an editorconfig file to configure codestyles.
    Added a Directory.Build.targets file to enforce Warning as Errors.

--- NumericCoreCalculator/Program.cs
+++ NumericCoreCalculator/Program.cs
@@ -1,3 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// © The Numeric Core Calculator contributors
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
 Console.WriteLine("Hello, World!");
